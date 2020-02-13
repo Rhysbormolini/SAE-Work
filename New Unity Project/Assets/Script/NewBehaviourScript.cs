c44c6884d2203da2fc7ade107f43f66dad827454
Rhysbormolini <61007759+Rhysbormolini@users.noreply.github.com>
2020-02-13 18:36:35 +0800

    Added Unity Project

---
+++ New Unity Project/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float movespeed = 10;
+    public float rotatespeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.transform.forward);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            this.transform.position += this.transform.forward * movespeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            this.transform.position -= this.transform.forward * movespeed * Time.deltaTime;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            transform.Rotate(Vector3.up * rotatespeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            transform.Rotate(Vector3.up * -rotatespeed * Time.deltaTime);
+        }
+
+
+        
+    }
+}
