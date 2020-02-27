c04b44aa03818b79baa284a9135c320d1dad1120
Rhysbormolini <61007759+Rhysbormolini@users.noreply.github.com>
2020-02-27 17:13:02 +0800

    ADDED A LOT
    included moving platforms, scoring, moving walls

---
+++ New Unity Project/Assets/Script/ResetLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) == true) { SceneManager.LoadScene(0); }
+    }
+}
